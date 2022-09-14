2f1bade515119eb820c8967f037c744a609ea9ca
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-15 01:28:30 +0300

    completed hooks refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,59 @@
-import React, { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
+import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { SearchBar, ImageGallery } from 'components';
+import { SearchBar, ImageGallery} from 'components';
+import { fetchImages } from 'helpers';
 
+const IMAGES_PER_PAGE = 20;
 
 export const App = () => {
   const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [isLoadBtnShown, setIsLoadBtnShown] = useState(true);
+ 
+  
+  useEffect(() => {
+    if (query) {
+      setStatus('pending');
+  
+      fetchImages(query, page).then((res) => {
+        if (!res.hits.length) {
+          setStatus('idle');
+          toast.error('Oops, seems like there is nothing found... Try another search,please!');
+          setImages([])
+          return;
+        }
+        const canLoadMoreImages = res.totalHits > IMAGES_PER_PAGE && ((res.totalHits / page) > IMAGES_PER_PAGE);
+        setStatus('resolved');
+        setImages(prevState => (page === 1 ? res.hits : [...prevState, ...res.hits]));
+        setIsLoadBtnShown(canLoadMoreImages)
+       
+      }).catch(() =>
+        setStatus('rejected'))
+
+    }
+    }, [page, query])
+ 
+  
 
   const handleFormSubmit = newQuery => {
-      setQuery(newQuery);
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
   };
+
+   const handleLoadMore = () => {
+    setPage(prevState => 
+      prevState + 1)
+  };
+
 
     return (
       <div className="app">
         <SearchBar onFormSubmit={handleFormSubmit} />
-        <ImageGallery query={query}  />
+        <ImageGallery images={images} status={status} isLoadBtnShown={isLoadBtnShown} onLoadMore={ handleLoadMore} /> 
         <ToastContainer
           position="top-right"
           theme="colored"
--- src/helpers/Api/FetchImages.js
+++ src/helpers/Api/FetchImages.js
@@ -3,7 +3,7 @@ const QUERYURL =
   'https://pixabay.com/api/?key=29314851-8b512a5abc572021537d02a85';
 
 export const fetchImages = async (query, page) => {
-  console.log(page)
+
      try{ const result = await axios.get(`${QUERYURL}&per_page=20&q=${query}&page=${page}`);
          return result.data;      
      }
